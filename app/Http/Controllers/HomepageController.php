fd142e3f0bacc47bd34a1843865d1ca2073c2008
dazmy <adamzamzam571@gmail.com>
2023-07-16 12:49:44 +0700

    edit filter for deposit and credit

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -16,8 +16,16 @@ class HomepageController extends Controller
     public function __invoke(Request $request)
     {
         $user = auth()->user()->load('grade');
-        $deposit = Deposit::orderBy('created_at', 'desc')->take(3)->get()->load('student');
-        $credit = Credit::orderBy('created_at', 'desc')->take(3)->get()->load('student');
+
+        $deposit = Deposit::orderBy('created_at', 'desc')->get()->load('student');
+        $deposit = $deposit->filter(function ($item) {
+            return $item->student->grade_id == auth()->user()->grade_id;
+        })->take(3);
+        
+        $credit = Credit::orderBy('created_at', 'desc')->get()->load('student');
+        $credit = $credit->filter(function ($item) {
+            return $item->student->grade_id == auth()->user()->grade_id;
+        })->take(3);
 
         return new ApiResource(true, 'Homepage', [
             'user' => $user,
